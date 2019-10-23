eaea70bd09992eb6fc3046b2b7e0b4306b3c238f
Janne Lavila <janne.lavila@wheelq.com>
2019-10-23 15:08:10 +0300

    implement 3.1 - 3.5

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,74 @@
-import * as http from 'http'
+import express from 'express'
+import bodyparser from 'body-parser'
 
-const app = http.createServer((req, res) => {
-  res.writeHead(200, { 'Content-Type': 'text/plain' })
-  res.end('Hello asdasdas')
+const app = express()
+app.use(bodyparser.json())
+
+let persons = [
+  {
+    "name": "Ada Lovelace",
+    "number": "39-44-5323523",
+    "id": 2
+  },
+  {
+    "name": "Dan Abramov",
+    "number": "12-43-234345",
+    "id": 3
+  },
+  {
+    "name": "Mary Poppendieck",
+    "number": "39-23-6423122",
+    "id": 4
+  },
+  {
+    "name": "rwarin",
+    "number": "aisfpasn",
+    "id": 5
+  },
+  {
+    "name": "asd",
+    "number": "321",
+    "id": 6
+  }
+]
+
+function nextId() {
+  return Math.ceil(Math.random() * 1000000000)
+}
+
+app.get('/info', (req, res) => {
+  res.send(`<p>Phonebook has info for 4 people</p><p>${new Date()}</p>`)
 })
 
-const port = 3001
-app.listen(port)
-console.log(`Server running on ${port}`)
+app.get('/api/', (req, res) => {
+  res.send('<h1>Hello world</h1>')
+})
+
+app.get('/api/persons', (req, res) => {
+  res.json(persons)
+})
+
+app.get('/api/persons/:id', (req, res) => {
+  const person = persons.find(person => person.id === Number(req.params.id))
+  if (person)
+    res.json(person)
+  else
+    res.status(404).end()
+})
+
+app.delete('/api/persons/:id', (req, res) => {
+  persons = persons.filter(person => person.id !== Number(req.params.id))
+  res.status(204).end()
+})
+
+app.post('/api/persons', (req, res) => {
+  const person = req.body as Person
+  person.id = nextId()
+  persons = persons.concat(person)
+  res.json(person)
+})
+
+const PORT = 3001
+app.listen(PORT, () => {
+  console.log(`Server running on ${PORT}`)
+})
