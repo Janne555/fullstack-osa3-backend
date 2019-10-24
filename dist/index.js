f060b98acd99539dd91f73c1744fa048f908dfd7
Janne Lavila <janne.lavila@wheelq.com>
2019-10-24 19:14:20 +0300

    compiled js

--- dist/index.js
+++ dist/index.js
@@ -1,12 +1,22 @@
 "use strict";
+var __importStar = (this && this.__importStar) || function (mod) {
+    if (mod && mod.__esModule) return mod;
+    var result = {};
+    if (mod != null) for (var k in mod) if (Object.hasOwnProperty.call(mod, k)) result[k] = mod[k];
+    result["default"] = mod;
+    return result;
+};
 var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
-const express_1 = __importDefault(require("express"));
+const express_1 = __importStar(require("express"));
 const body_parser_1 = __importDefault(require("body-parser"));
 const morgan_1 = __importDefault(require("morgan"));
 const cors_1 = __importDefault(require("cors"));
+const dotenv_1 = __importDefault(require("dotenv"));
+dotenv_1.default.config();
+const person_1 = __importDefault(require("./models/person"));
 morgan_1.default.token('post_body', (req, res) => {
     return req.headers["content-type"] && req.headers["content-type"].includes("application/json") ? JSON.stringify(req.body) : "";
 });
@@ -15,33 +25,6 @@ app.use(express_1.default.static('build'));
 app.use(cors_1.default());
 app.use(morgan_1.default(':method :url :status :res[content-length] - :response-time ms :post_body'));
 app.use(body_parser_1.default.json());
-let persons = [
-    {
-        "name": "Ada Lovelace",
-        "number": "39-44-5323523",
-        "id": 2
-    },
-    {
-        "name": "Dan Abramov",
-        "number": "12-43-234345",
-        "id": 3
-    },
-    {
-        "name": "Mary Poppendieck",
-        "number": "39-23-6423122",
-        "id": 4
-    },
-    {
-        "name": "rwarin",
-        "number": "aisfpasn",
-        "id": 5
-    },
-    {
-        "name": "asd",
-        "number": "321",
-        "id": 6
-    }
-];
 function nextId() {
     return Math.ceil(Math.random() * 1000000000);
 }
@@ -52,51 +35,58 @@ app.get('/api/', (req, res) => {
     res.send('<h1>Hello world</h1>');
 });
 app.get('/api/persons', (req, res) => {
-    res.json(persons);
+    person_1.default.find({})
+        .then(persons => {
+        console.log(persons.map(person => person.toJSON()));
+        res.json(persons.map(person => person.toJSON()));
+    })
+        .catch(err => {
+        console.error(err);
+        res.status(500).end();
+    });
 });
 app.get('/api/persons/:id', (req, res) => {
-    const person = persons.find(person => person.id === Number(req.params.id));
-    if (person)
-        res.json(person);
-    else
-        res.status(404).end();
+    person_1.default.findById(req.params.id)
+        .then(person => {
+        express_1.response.json(person.toJSON());
+    });
 });
-app.delete('/api/persons/:id', (req, res) => {
-    persons = persons.filter(person => person.id !== Number(req.params.id));
-    res.status(204).end();
-});
+// app.delete('/api/persons/:id', (req, res) => {
+//   persons = persons.filter(person => person.id !== Number(req.params.id))
+//   res.status(204).end()
+// })
 app.post('/api/persons', (req, res) => {
-    const person = req.body;
-    person.id = nextId();
-    persons = persons.concat(person);
-    res.json(person);
+    if (!req.body)
+        return express_1.response.status(400).json({ error: 'content missing' });
+    const person = new person_1.default(Object.assign({}, req.body));
+    person.save()
+        .then(savedPerson => {
+        res.json(savedPerson.toJSON());
+    });
 });
-app.put('/api/persons/:id', (req, res) => {
-    let person = req.body;
-    const id = Number(req.params.id);
-    if (person.name) {
-        if (person.number) {
-            const index = persons.findIndex(p => p.id === id);
-            persons[index] = Object.assign(Object.assign({}, persons[index]), { number: person.number });
-            res.json(persons[index]);
-        }
-        else {
-            if (persons.some(p => p.name === person.name)) {
-                res.status(400);
-                return res.json({ error: "name must be unique" });
-            }
-            else {
-                person = { id: nextId(), name: person.name };
-                persons = persons.concat(person);
-                res.json(person);
-            }
-        }
-    }
-    else {
-        res.status(400);
-        return res.json({ error: "missing name" });
-    }
-});
+// app.put('/api/persons/:id', (req, res) => {
+//   let person = req.body as Person
+//   const id = Number(req.params.id)
+//   if (person.name) {
+//     if (person.number) {
+//       const index = persons.findIndex(p => p.id === id)
+//       persons[index] = { ...persons[index], number: person.number }
+//       res.json(persons[index])
+//     } else {
+//       if (persons.some(p => p.name === person.name)) {
+//         res.status(400)
+//         return res.json({ error: "name must be unique" })
+//       } else {
+//         person = { id: nextId(), name: person.name }
+//         persons = persons.concat(person)
+//         res.json(person)
+//       }
+//     }
+//   } else {
+//     res.status(400)
+//     return res.json({ error: "missing name" })
+//   }
+// })
 const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => {
     console.log(`Server running on ${PORT}`);
---
+++ dist/models/person.js
@@ -0,0 +1,28 @@
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const mongoose_1 = __importDefault(require("mongoose"));
+const url = process.env.MONGODB_URI;
+console.log("connecting to", url);
+mongoose_1.default.connect(url, { useNewUrlParser: true })
+    .then(result => {
+    console.log("connected to MongoDB");
+})
+    .catch(error => {
+    console.error('error connecting to MongoDB:', error.message);
+});
+const personSchema = new mongoose_1.default.Schema({
+    name: String,
+    number: String
+});
+personSchema.set('toJSON', {
+    transform: (document, returnedObject) => {
+        returnedObject.id = returnedObject._id.toString();
+        delete returnedObject._id;
+        delete returnedObject.__v;
+    }
+});
+exports.default = mongoose_1.default.model('Person', personSchema);
+//# sourceMappingURL=person.js.map
