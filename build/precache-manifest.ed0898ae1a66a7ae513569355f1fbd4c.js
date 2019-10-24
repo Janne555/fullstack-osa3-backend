901a3f68236f2555e7f35088730c2a5ed770bc62
Janne Lavila <janne.lavila@wheelq.com>
2019-10-24 17:19:04 +0300

    include ui

---
+++ build/precache-manifest.ed0898ae1a66a7ae513569355f1fbd4c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c7a6c806a74e11663c2fdf863b788638",
+    "url": "/index.html"
+  },
+  {
+    "revision": "209b06d7a344fc1d894b",
+    "url": "/static/css/main.6ba95082.chunk.css"
+  },
+  {
+    "revision": "b85a6b39234ab5863594",
+    "url": "/static/js/2.28b26038.chunk.js"
+  },
+  {
+    "revision": "209b06d7a344fc1d894b",
+    "url": "/static/js/main.81e0b386.chunk.js"
+  },
+  {
+    "revision": "ef9959b79e3ec7a5633b",
+    "url": "/static/js/runtime-main.6448cf55.js"
+  }
+]);
