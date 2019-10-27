b230abf715e16b09709967f4a20592bed0ea0a56
Janne Lavila <janne.lavila@wheelq.com>
2019-10-27 19:09:13 +0200

    update compiled js

--- dist/index.js
+++ dist/index.js
@@ -3,6 +3,7 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
+//eslint-disable-next-line
 const express_1 = __importDefault(require("express"));
 const body_parser_1 = __importDefault(require("body-parser"));
 const morgan_1 = __importDefault(require("morgan"));
